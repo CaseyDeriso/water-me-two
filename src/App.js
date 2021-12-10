75a1bd69c09dfcf1b625b1d13ce5a666a27fe6ce
Casey Deriso <caseyderiso@gmail.com>
2021-12-09 19:27:03 -0600

    dumped html and css in a default react app with minimum to render a mock up of the site

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Plant from "./components/plant";
+
+function App() {
+	return (
+		<body>
+			<div class="top">
+				<div class="jumbotron">
+					<div class="title">
+						<h1> Water Me!</h1>
+						<p>
+							Add your plants to your profile page along with the amount
+							<br />
+							water each pot needs and how often you water it.
+						</p>
+					</div>
+
+					<div class="add">
+						<div class="add__container">
+							<input
+								type="text"
+								placeholder="Plant name."
+								class="pName"
+								autocomplete="off"
+							/>
+							<input
+								type="text"
+								placeholder="How much water?"
+								class="aWater"
+								autocomplete="off"
+							/>
+							<input
+								type="text"
+								placeholder="How often?"
+								class="often"
+								autocomplete="off"
+							/>
+							<button class="add___btn" onclick="clickAdd()">
+								add
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="bottom">
+				<div class="plant__container"><Plant></Plant></div>
+			</div>
+		</body>
+	);
+}
+
+export default App;
