d1021a97c9cff686b6d18d8b80042c5ce225b752
Casey Deriso <caseyderiso@gmail.com>
2021-12-16 16:32:07 -0600

    export user context to its own module

---
+++ src/utils/user.context.js
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { createContext } from "react";
+import { useUserReducer} from "./reducers";
+
+const UserContext = createContext([[],() => {}]);
+const { Provider } = UserContext
+
+// wrapper for the provider
+const UserProvider = ({ user, children }) => {
+  const [state, dispatch] = useUserReducer({
+    userName: '',
+    plants: []
+  });
+
+  return (
+    <Provider value={[state, dispatch]}>
+      {children}
+    </Provider>
+  )
+}
+
+const useUserContext = () => {
+  return useContext(UserContext)
+}
+
+export { UserProvider, useUserContext }
